b3ea5fca81d25ccf3232537259c260394514d724
Terren Culberson <culbersonterren@yahoo.com>
2020-05-26 21:16:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,10 @@ class Song
     song = self.new
     song.name = song_name
     song.artist_name = artist_name
-    song
-    end
+    song 
+  end
+  
+    def self.create_from_filename````
+    
 end
 
